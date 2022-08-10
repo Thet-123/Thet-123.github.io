056c7f0d386dd18aacb4137814ab5de65d31e6c9
Thet Htet Soe <thethtetsoe789@gmail.com>
2022-08-10 10:31:14 +0630

    Update YMHT

---
+++ js/slideroverlay.js
@@ -0,0 +1,47 @@
+
+// construction start
+
+var slidecontainer = document.getElementsByClassName("slider-container-construction")[0];
+
+
+function overlayOpenconstruction(){
+    slidecontainer.style.display = "flex";
+}
+
+function overlayCloseconstruction(){
+    slidecontainer.style.display = "none";
+}
+
+// construction end
+
+// interior start
+
+
+
+var slidecontainerinterior = document.getElementsByClassName("slider-container-interior")[0];
+
+
+function overlayOpeninterior(){
+    slidecontainerinterior.style.display = "flex";
+}
+
+function overlayCloseinterior(){
+    slidecontainerinterior.style.display = "none";
+}
+// interior end
+
+
+//upvc start
+
+
+var slidecontainerupvc = document.getElementsByClassName("slider-container-upvc")[0];
+
+
+function overlayOpenupvc(){
+    slidecontainerupvc.style.display = "flex";
+}
+
+function overlayCloseupvc(){
+    slidecontainerupvc.style.display = "none";
+}
+//upvc end
